feb2f122bd92e65d8a210c776892182cbcffcbeb
ATariq <atariq1@r1rcm.com>
2023-04-10 13:08:16 -0600

    Merged updates with my local changes for the navbar

--- src/app/scaffold/navbar/navbar.component.ts
+++ src/app/scaffold/navbar/navbar.component.ts
@@ -1,8 +1,35 @@
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    menuItems = [
+      // {
+      //   label: 'Home',
+      //   routerLink: "/welcome",
+      //   icon: "/assets/images/atla_elementsLogo.png"
+      // },
+      {
+        label: 'Air',
+        routerLink: "/elements/air"
+      },
+      {
+        label:'Water',
+        routerLink: "/elements/water"
+      },
+      {
+        label: 'Earth',
+        routerLink: "/elements/earth"
+      },
+      {
+        label: 'Fire',
+        routerLink: "/elements/fire"
+      }
+    ];
+
+}
